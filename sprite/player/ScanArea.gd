9e1d0cc84278c8ffc38f5f4ad44551a53d6eb3d6
SirRana <60571003+RanaIsHere@users.noreply.github.com>
2020-10-22 13:56:18 +0700

    Xenoterminated v0.6.3
    A little of polishing. Last build version for development, this is Game Jam release build.  Added Boss Music and fixed noise for ScanArea.

--- sprite/player/ScanArea.gd
+++ sprite/player/ScanArea.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
 var resSpeed = 600
-
+var damage = preload("res://scenes/DamageCounter.tscn").instance()
 
 func _ready():
 	pass
@@ -15,12 +15,15 @@ func _on_ScanArea_body_entered(body):
 		
 	if body.is_in_group("enemies") or body.is_in_group("neutral") or body.is_in_group("pinkofly"):
 		body.researchLimit -= 1
+		body.add_child(damage)
+		
 		if body.researchLimit != 1:
 			Globals.researchPoint += 1
 			body.queue_free()
 		queue_free()
 	if body.is_in_group("boss"):
 		body.health -= 1
+		body.add_child(damage)
 		print(body.health)
 		
 		if body.health < 0 or body.health == 0 or body.health <= 0:
